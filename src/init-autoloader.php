3133ce24e9d7be3089a9262000c485402f6842c0
TOGoS <togos00@gmail.com>
2010-07-22 15:23:04 -0500

    Added some smarts to the autoloader so that it can find legacy (Underscore_Delimited_Namespaces) classes (e.g. PHPUnit_Framework_TestCase) when using the namespaced name.

--- src/init-autoloader.php
+++ src/init-autoloader.php
@@ -2,4 +2,8 @@
 
 function __autoload($className) {
 	require_once str_replace('\\','/',$className).'.php';
+	$usClassName = str_replace('\\','_',$className);
+	if( !class_exists($className,false) && class_exists($usClassName,false) ) {
+		class_alias($usClassName,$className);
+	}
 }
--- test/PHPUnit/Framework/TestCase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace PHPUnit\Framework;
-
-require_once 'PHPUnit/Framework.php';
-
-class TestCase extends \PHPUnit_Framework_TestCase {}
