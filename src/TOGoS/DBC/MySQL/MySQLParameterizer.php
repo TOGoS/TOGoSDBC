f794e2c45810e909a58b4ad8559cb9c26a8cfa5e
Dan Stevens <stevens@robert.earthit.com>
2012-12-04 21:26:20 -0600

    MySQLParameterizer's constructor now takes a link argument which is passed to sqlEncodeString.

--- src/TOGoS/DBC/MySQL/MySQLParameterizer.php
+++ src/TOGoS/DBC/MySQL/MySQLParameterizer.php
@@ -2,13 +2,13 @@
 
 class TOGoS_DBC_MySQL_MySQLParameterizer extends TOGoS_DBC_Util_Parameterizer
 {
-	public static $instance;
-	public static function getInstance() {
-		if( self::$instance === null ) self::$instance = new self();
-		return self::$instance;
+	protected $link;
+	
+	public function __construct( $link ) {
+		$this->link = $link;
 	}
 	
 	protected function sqlEncodeString( $value ) {
-		return "'".mysql_real_escape_string($value)."'";
+		return "'".mysql_real_escape_string($value, $this->link)."'";
 	}
 }
